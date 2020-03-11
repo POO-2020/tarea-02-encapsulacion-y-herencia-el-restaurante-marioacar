f5764b4092e488f3abcb3edc607646a974b877bb
marioacar <mcarvantes@ucol.mx>
2020-03-11 16:39:01 -0600

    ClienteF

--- elementoPedido.js
+++ elementoPedido.js
@@ -2,11 +2,11 @@ import Producto from "producto.js"
 export default class ElementoPedido{
 
     constructor(Producto = new Producto, cantidad){
-        this.Producto = Producto
-        this.cantidad = cantidad
+        this._Producto = Producto
+        this._cantidad = cantidad
     }
 
     getDescripcion(){
-        return `${this.cantidad} x ${this.Producto.getDescripcion()}`
+        return `${this._cantidad} x ${this._Producto.getDescripcion()}`
     }
 }
